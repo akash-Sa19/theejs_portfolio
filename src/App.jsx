c5fb9ad5c76b643b2bd96792091e5c5401e11cf3
Akash Sahu <akashsahu793@gmail.com>
2024-09-09 23:54:33 +0530

    just installed three.js and other subsidiary packages
    1:52 -> all good

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./section/navbar";
+import Hero from "./section/Hero";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
